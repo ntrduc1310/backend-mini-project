2ac9d5844b4e472b629e857f5b4e32893051a219
Ducci <2251010020duc@ou.edu.vn>
2025-08-09 21:12:54 +0700

    Add GameNameDto.java

--- gameapp/src/main/java/com/example/gameapp/dto/GameNameDto.java
+++ gameapp/src/main/java/com/example/gameapp/dto/GameNameDto.java
@@ -1,5 +1,20 @@
 package com.example.gameapp.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.example.gameapp.model.Language;
+
 public class GameNameDto {
-    
+    @NotNull
+    private Language language;
+
+    @NotBlank
+    private String value;
+
+    // Getters & Setters
+    public Language getLanguage() { return language; }
+    public void setLanguage(Language language) { this.language = language; }
+    public String getValue() { return value; }
+    public void setValue(String value) { this.value = value; }
 }
